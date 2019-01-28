166152418dcb4af79dfded1bbba455d21eb898bf
Mariko <maripiyok@gmail.com>
2019-01-28 20:14:49 +0900

    layout

---
+++ layouts/single.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <span>Single layout</span>
+    <AppNavigation />
+    <hr>
+    <nuxt />
+    <nuxt-link to="/">Back to Top</nuxt-link>
+    <hr>
+    <footer>
+      footer 
+    </footer>
+  </div>
+</template>
+
+
+<script>
+import AppNavigation from '~/components/AppNavigation.vue'
+
+export default {
+  components: {
+    AppNavigation
+  }
+}
+</script>
