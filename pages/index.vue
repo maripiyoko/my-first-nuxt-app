ec53f718b443ec384cfb90b755dc09708785b71e
Mariko <maripiyok@gmail.com>
2019-01-29 20:15:31 +0900

    Add pages for login.

--- pages/index.vue
+++ pages/index.vue
@@ -3,21 +3,16 @@
     <div>
       <app-logo/>
       <h1 class="title">
-        my-first-nuxt-app
+        Index
       </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      <ul>
+        <li>
+          <nuxt-link to='/login'>Login page</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to='authed-route'>Go to the page need authorization!</nuxt-link>
+        </li>
+      </ul>
     </div>
   </section>
 </template>
